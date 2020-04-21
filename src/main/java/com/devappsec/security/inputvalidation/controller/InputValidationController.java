873b2884e3db76a84d59f40ddf0449908dd7ce64
D3n0Duz <denoduz@gmail.com>
2020-04-21 14:25:40 -0400

    clean  http responses

--- src/main/java/com/devappsec/security/inputvalidation/controller/InputValidationController.java
+++ src/main/java/com/devappsec/security/inputvalidation/controller/InputValidationController.java
@@ -2,10 +2,13 @@ package com.devappsec.security.inputvalidation.controller;
 
 import com.devappsec.security.inputvalidation.model.InputValidationModel;
 import com.devappsec.security.inputvalidation.service.InputValidationServiceImpl;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class InputValidationController {
@@ -22,21 +25,37 @@ public class InputValidationController {
     public ResponseEntity<Void> validateMany(@RequestBody InputValidationModel[] requestInputValidationModel) throws Exception{
 
         int isNotSafeCounter = 0;
+        int currentIndex = 0;
+        ArrayList<Integer> indexWithError = new ArrayList<Integer>();
 
         if(requestInputValidationModel == null) {
             return ResponseEntity.noContent().build();
         }
 
         for(InputValidationModel singleInput : requestInputValidationModel) {
+            currentIndex += 1;
             if(!inputValidationService.validateInput(singleInput)) {
+                indexWithError.add(currentIndex);
                 isNotSafeCounter += 1;
             }
         }
 
         if(isNotSafeCounter == 0) {
-            return new ResponseEntity("Safe", HttpStatus.OK);
+            JSONObject responseObj = new JSONObject();
+
+            responseObj.put("title", "OK");
+            responseObj.put("status", 200);
+
+            return new ResponseEntity(responseObj.toString(), HttpStatus.OK);
         } else {
-            return new ResponseEntity("Not safe",HttpStatus.NOT_FOUND);
+            JSONObject responseObj = new JSONObject();
+
+            responseObj.put("title", "Bad Request");
+            responseObj.put("status", 400);
+            responseObj.put("type", "/validates");
+            responseObj.put("invalidFields", "Unsafe value for the type in position "+ indexWithError.toString() +".");
+
+            return new ResponseEntity(responseObj.toString(),HttpStatus.BAD_REQUEST);
         }
 
     }
